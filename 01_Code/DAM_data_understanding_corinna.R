019b1cbcb56a8bd9bdaa364fb4dcb6700a102a6b
Corinna Maher Mittmann <mittmann.corinna@gmail.com>
2017-09-25 15:31:33 +1000

    Finished cleaning
    1 ID varaible removed
    8 single level variables removed
    2 almost single level variables with 0 and NA as only values removed
    
    reformatted date

--- 01_Code/DAM_data_understanding_corinna.R
+++ 01_Code/DAM_data_understanding_corinna.R
@@ -40,6 +40,8 @@ library(nnet)
 source(paste0(code_file_path,"99_general_functions_DAM_Corinna.R"))
 # h2o package for neural nets
 
+#### ==== Pre-clean exploration
+
 ### 
 default <- read.csv(paste0(Original_Data_Location,"training.csv"), header = T)
 data_dict <- read.csv(paste0(Original_Data_Location,"data_dictionary.csv"), header = T)
@@ -59,6 +61,8 @@ descriptive_info(default, "loan_status")
 
 str(default)
 
+## === START CLEAN
+
 # remove columns with 1 level
 
 mylist_levels <- default %>%
@@ -70,11 +74,24 @@ typeof(cols_to_keep)
 typeof(names(default))
 
 default2 = default[,cols_to_keep]
-# rewrite working data
-write.csv(default2, paste0(working_file_path,"clean_data_v1.csv"),row.names = F)
 
-data<- read.csv(paste0(working_file_path,"clean_data_v1.csv"))
-# Refomat the date column
+### ======== may need to review this decision to remove as 0 and NA values only - ? does distinction btw 0 and NA have value?
+#collections # n.a
+unique(default2$collections_12_mths_ex_med)
+table(default2$collections_12_mths_ex_med)
+default2 <- select(default2, -collections_12_mths_ex_med)
+# remove tax liens
+unique(default2$tax_liens)
+table(default2$tax_liens)
+default2 <- select(default2, -tax_liens)
+
+# rewrite working data
+# default = default[,cols_to_keep]
+# write.csv(default, paste0(working_file_path,"clean_data_v1.csv"),row.names = F)
+# 
+# write.csv(default2, paste0(working_file_path,"clean_data_31var.csv"),row.names = F)
+# data<- read.csv(paste0(working_file_path,"clean_data_31var.csv"))
+
 
 # make interest rate numeric
 unique(data$int_rate)
@@ -85,7 +102,7 @@ n_distinct((substr(data$int_rate, 1,nchar(data$int_rate)-1))) == n_distinct(data
 
 
 data$int_rate <- substr(data$int_rate, 1,nchar(data$int_rate)-1)
-write.csv(data, paste0(working_file_path,"clean_data_v2.csv"),row.names = F)
+# write.csv(data, paste0(working_file_path,"clean_data_v2.csv"),row.names = F)
 data<- read.csv(paste0(working_file_path,"clean_data_v2.csv"))
 str(data)
 
@@ -95,16 +112,74 @@ data$emp_length <- gsub("\\.","",data$emp_length)
 head(data$emp_length)
 # deal with na
 table(data$emp_length == "na")
-data$emp_length <- ifelse(data$emp_length == "na","XUK",data$emp_length)
+data$emp_length <- ifelse(data$emp_length == "na","XUK",data$emp_length) #XUK for unknown values we can change this to "unknown"
 table(data$emp_length == "XUK")
 # keep only the value and remove "years" year, ..years etc units
 data$emp_length <- gsub("year","",data$emp_length)
 data$emp_length <- gsub("s","",data$emp_length)
+unique(data$emp_length)
+qplot(data$emp_length, fill = "identity")
+# write.csv(data, paste0(working_file_path,"clean_data_v3.csv"),row.names = F)
+# data<- read.csv(paste0(working_file_path,"clean_data_v3.csv"))
 
-write.csv(data, paste0(working_file_path,"clean_data_v3.csv"),row.names = F)
-data<- read.csv(paste0(working_file_path,"clean_data_v3.csv"))
+# Make term consistent
+# change missing format
+data$term <- gsub("\\.","",data$term)
+head(data$term)
+# deal with na
+table(data$term == "na")
+# no na values established
+# data$term <- ifelse(data$term == "na","XUK",data$term)
+# table(data$term == "XUK")
+# keep only the value and remove "years" year, ..years etc units
+data$term <- gsub("month","",data$term)
+data$term <- gsub("s","",data$term)
+head(data$term)
+unique(data$term)
+qplot(data$term, fill = "identity")
+# write.csv(data, paste0(working_file_path,"clean_data_v4.csv"),row.names = F)
+# data<- read.csv(paste0(working_file_path,"clean_data_v4.csv"))
 
+# Make verification_status consistent
+# change missing format
+unique(data$verification_status)
+data$verification_status <- gsub("\\.","_",data$verification_status)
+qplot(data$verification_status, fill = "identity")
+# pub rec bankruptcies # NA
+unique(data$pub_rec_bankruptcies)
+# deal with na
+table(is.na(data$pub_rec_bankruptcies))
+data$pub_rec_bankruptcies <- ifelse(is.na(data$pub_rec_bankruptcies),"XUK",data$pub_rec_bankruptcies)
+table(data$pub_rec_bankruptcies == "XUK")
+as.factor(data$pub_rec_bankruptcies)
+qplot(data$pub_rec_bankruptcies, fill = "identity")
+table(data$pub_rec_bankruptcies)
 
+## ====== Refomat the date column
+
+# earliest cred line convert to floor date
+data$earliest_cr_line = gsub("\\.","-",data$earliest_cr_line)
+data <- mutate(data, earliest_cr_line = as.Date(paste0("01-",earliest_cr_line), format = "%d-%b-%Y"))
+# test <- dplyr::sample_n(as.tibble(data$earliest_cr_line), 10)
+unique(data$earliest_cr_line)
+# qplot(data$earliest_cr_line, data$total_rec_int, colour = data$inq_last_6mths)
+qplot(data$earliest_cr_line, fill = "identity") ## + facet_wrap(data$loan_status)
+## we may or may not want to convert the date into continuous time series
+## it may be advantageous to keep the time format as is
+
+###########################################
 
 # Remove ID to rownames before moedelling
-data$member_id
+head(data$member_id)
+row.names(data) <- data$member_id
+data <- select(data, -member_id)
+head(data)
+tail(data)
+tail(default)
+## note the above transofrmation is only available whilst member id is unique
+
+# write.csv(data, paste0(working_file_path,"clean_data_v5.csv"),row.names = T)
+# write.csv(data, paste0(working_file_path,"clean_data_30var.csv"),row.names = T)
+data <- read.csv(paste0(working_file_path,"clean_data_30var.csv"), row.names = T)
+
+### === END CLEAN
